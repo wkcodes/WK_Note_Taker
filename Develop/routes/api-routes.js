5b5c046846d327415ba16f78115a79457c343702
Will Kendall <will@wills-MacBook-Pro.local>
2020-10-27 22:07:03 -0700

    Added functionality to give notes ids, added functionality to delete notes. App may be complete at this point.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 let rawData = fs.readFileSync("./db/db.json");
 let data = JSON.parse(rawData)
 
+//Create id 
+var idCounter = -1;
+
 console.log(data) 
 
 //Routing
@@ -11,17 +14,31 @@ module.exports = function(app) {
     //Returns all the notes
     app.get("/api/notes", (req, res) => {
         res.json(data);
-        //console.log(noteData);
     });
 
     //Saves a new note
     app.post("/api/notes", (req, res) => {
+        //Add id
+        idCounter++;
+        req.body["id"] = idCounter;
         //Write the req to notes
         data.push(req.body);
         fs.writeFile("./db/db.json", JSON.stringify(data), err => {
             if(err) throw err;
         });
         res.json(true);
-    })
+    });
+
+    //Delete a note 
+    app.delete("/api/notes/:id", (req, res) => {
+        //Figure out giving posts ids
+        let targetNote = req.params['id'];
+        data.splice(targetNote, 1);
+        fs.writeFile("./db/db.json", JSON.stringify(data), err => {
+            if(err) throw err;
+        });
+        
+        console.log(data)
+    });
 }
 
