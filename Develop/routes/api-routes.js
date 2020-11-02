b7009f7086ab255881c1587a853c136d960c3cdb
Will Kendall <will@wills-MacBook-Pro.local>
2020-11-02 12:19:05 -0800

    added gitignore with node modules

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,42 +1,39 @@
  //Load data 
-const fs = require('fs');
-let rawData = fs.readFileSync("./db/db.json");
-let data = JSON.parse(rawData)
+ const fs = require('fs');
+ let rawData = fs.readFileSync("./db/db.json");
+ let data = JSON.parse(rawData)
 
-//Create id variable for notes
-var idCounter = -1;
+ //Routing
+ module.exports = function (app) {
 
-//Routing
-module.exports = function(app) {
+     //Returns all the notes
+     app.get("/api/notes", (req, res) => {
+         res.json(data);
+     });
 
-    //Returns all the notes
-    app.get("/api/notes", (req, res) => {
-        res.json(data);
-    });
+     //Saves a new note
+     app.post("/api/notes", (req, res) => {
+         //Assign id
+         req.body["id"] = data.length + 1;
+         console.log(req.body)
+         //Write the req to notes
+         data.push(req.body);
+         fs.writeFile("./db/db.json", JSON.stringify(data), err => {
+             if (err) throw err;
+         });
+         res.json(true);
+     });
 
-    //Saves a new note
-    app.post("/api/notes", (req, res) => {
-        //Add id
-        idCounter++;
-        req.body["id"] = idCounter;
-        //Write the req to notes
-        data.push(req.body);
-        fs.writeFile("./db/db.json", JSON.stringify(data), err => {
-            if(err) throw err;
-        });
-        res.json(true);
-    });
-
-    //Delete a note 
-    app.delete("/api/notes/:id", (req, res) => {
-        //Gets index of note to be deleted
-        let targetNote = req.params['id'];
-        //Deletes note
-        data.splice(targetNote, 1);
-        //Writes changes to file
-        fs.writeFile("./db/db.json", JSON.stringify(data), err => {
-            if(err) throw err;
-        });
-    });
-}
-
+     //Delete a note 
+     app.delete("/api/notes/:id", (req, res) => {
+         //Gets index of note to be deleted
+         let targetNote = (req.params['id'] -1);
+         console.log(targetNote);
+         //Deletes note
+         data.splice(targetNote, 1);
+         //Writes changes to file
+         fs.writeFile("./db/db.json", JSON.stringify(data), err => {
+             if (err) throw err;
+         });
+     });
+ }
